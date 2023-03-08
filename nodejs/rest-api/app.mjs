aee6113bf6caf10e4b8d776d94a5f096caef7a3c
Derrick Nnaji <dnnaji@gmail.com>
2023-03-08 15:07:12 -0600

    Simple API

--- nodejs/rest-api/app.mjs
+++ nodejs/rest-api/app.mjs
@@ -1,45 +1,11 @@
 import express from "express";
-import cache from "./cache.mjs";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/keys/:key", (req, res) => {
-  const key = req.params.key;
-  if (!key || typeof key !== "string") {
-    return res.status(400).json({ error: "missing or invalid key" });
-  }
-  if (!cache.has(key)) {
-    return res.status(404).json({ error: "key does not exist" });
-  }
-  const value = cache.get(key);
-  return res.json({ key: id, value });
-});
-
-app.get("/api/keys", (_, res) => {
-  return res.json(cache.keys());
-});
-
-app.post("/api/keys", (req, res) => {
-  const { key, value, ttl } = req.body;
-  if (!key || !value) {
-    return res.status(400).json({ error: "invalid request body" });
-  }
-  cache.set(key, value, ttl ?? 0);
-  return res.status(201).json({ key });
-});
-
-app.delete("/api/keys/:key", (req, res) => {
-  const key = req.params.key;
-  if (!key || typeof key !== "string") {
-    return res.status(400).json({ error: "missing or invalid key" });
-  }
-  if (!cache.has(key)) {
-    return res.status(404).json({ error: "key does not exist" });
-  }
-  cache.del(key);
-  return res.json({ key });
+app.get("/hello", (req, res) => {
+  return res.json({ message: "Hello World!" });
 });
 
 app.get("/healthz", (_, res) => {
